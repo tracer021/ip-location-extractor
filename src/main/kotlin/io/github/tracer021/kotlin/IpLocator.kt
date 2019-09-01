eaaaf9dfae4709299ce334b1c6accfee158f22f2
tracer021 <tracer021@gmail.com>
2019-09-01 20:31:09 +0800

    Refactor

--- src/main/kotlin/io/github/tracer021/kotlin/IpLocator.kt
+++ src/main/kotlin/io/github/tracer021/kotlin/IpLocator.kt
@@ -2,28 +2,23 @@ package io.github.tracer021.kotlin
 
 import io.restassured.RestAssured
 
-object IpLocator {
 
+fun main(args: Array<String>) {
+    System.err.close();
+    System.setErr(System.out);
 
-        @JvmStatic
-        fun main(args: Array<String>) {
-            System.err.close();
-            System.setErr(System.out);
+    val start = System.currentTimeMillis()
+    val accessKey = System.getenv("accessKey");
 
-            val start = System.currentTimeMillis()
-            val accessKey = System.getenv("accessKey");
+    val url = "http://api.ipstack.com/check?access_key=$accessKey"
+    val jsonPath = RestAssured.given()
+            .get(url)
+            .jsonPath()
 
-            val url = "http://api.ipstack.com/check?access_key=$accessKey"
-            val jsonPath = RestAssured.given()
-                    .get(url)
-                    .jsonPath()
-
-            val ip = jsonPath.get<String>("ip")
-            val country = jsonPath.get<String>("country_name")
-            val city = jsonPath.get<String>("city")
-            println("IP: $ip Country is $country, City: $city")
-            val diff = (System.currentTimeMillis() - start) / 1000
-            println("Took $diff seconds")
-        }
-
+    val ip = jsonPath.get<String>("ip")
+    val country = jsonPath.get<String>("country_name")
+    val city = jsonPath.get<String>("city")
+    println("IP: $ip Country is $country, City: $city")
+    val diff = (System.currentTimeMillis() - start) / 1000
+    println("Took $diff seconds")
 }
